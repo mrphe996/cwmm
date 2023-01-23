44010ad5baad9c0404488f225b60496da093e5ac
John Doe <thid>
2023-01-23 15:07:56 +0700

    mung22

--- components/PageHead.tsx
+++ components/PageHead.tsx
@@ -38,11 +38,6 @@ export const PageHead: React.FC<
           <meta property='twitter:domain' content={site.domain} />
         </>
       )}
-
-      {config.twitter && (
-        <meta name='twitter:creator' content={`@${config.twitter}`} />
-      )}
-
       {description && (
         <>
           <meta name='description' content={description} />
